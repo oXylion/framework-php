6f725c798d994f7463d3f8bc27df0d77a207b1b1
sharpix <modern.codev@gmail.com>
2014-07-21 01:12:02 +0200

    init & update
    init project frot controller,  composer files

---
+++ web/app_dev.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is part of the PHPCodev package
+ *                      created at: 21.07.14
+ *
+ * Copyright (c) oXylion 2014
+ *     sharpix  <modern.codev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+print "hello world!";
