0d683add85b348aa953e10edcf40002177321342
sharpix <modern.codev@gmail.com>
2014-07-21 13:01:33 +0200

    init fix

--- web/app.php
+++ web/app.php
@@ -2,7 +2,6 @@
 
 // - Register use namespace
 use \BoostFactor\ActionDummy;
-use \BoostFactor\DummyTwig;
 
 // - Service autoloader  with boostrap module register.
 $loader = require_once( dirname( __DIR__ ) . '/app' . '/bootstrap.php' );
