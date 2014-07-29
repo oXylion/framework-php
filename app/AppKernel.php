31d1a151691b07b7b14950c0147e8905eebe3902
sharpix <modern.codev@gmail.com>
2014-07-29 02:03:19 +0200

    init

---
+++ app/AppKernel.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the oXylion framework.
+ *
+ * (c) sharpix - Bartosz Zwierzchowski, Poland 
+ *               <bartosz.zwierzchowski95@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+class AppKernel
+{
+}
